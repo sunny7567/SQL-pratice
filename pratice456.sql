45e955d6610457f6ac8aa22bc9cd3412394909f8
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 21:49:14 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -67,3 +67,9 @@ SELECT total_amt_usd, id, account_id
 FROM orders
 ORDER BY total_amt_usd DESC
 LIMIT 5;
+              
+--Write a query to return the lowest 20 orders in terms of the smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders 
+ORDER BY total_amt_usd
+LIMIT 20;
