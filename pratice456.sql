3d4c20ae13a94323aad68a1b69b4eefe5e32c8c5
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 22:15:55 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -101,4 +101,9 @@ LIMIT 10;
 SELECT name, website, primary_poc
 FROM accounts
 WHERE name = 'Exxon Mobil';
+              
+--Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+SELECT id, account_id, (standard_amt_usd/standard_qty) AS unit_price
+FROM orders
+LIMIT 10;
 
