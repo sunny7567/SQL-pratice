0d69ef7ac3358d2ff6f7846a9e2b1edfc648e6c3
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 22:05:31 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -85,3 +85,8 @@ FROM orders
 ORDER BY  total_amt_usd DESC, account_id;
               
 
+--Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
