e8994016be94f6b8330e12227718ac2da24eea35
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 22:07:50 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -90,3 +90,9 @@ SELECT *
 FROM orders
 WHERE gloss_amt_usd >= 1000
 LIMIT 5;
+              
+--Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
