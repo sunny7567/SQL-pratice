9afd18b046e203396ae2e3065c2f3208a1ed89ed
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 22:11:39 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -96,3 +96,9 @@ SELECT *
 FROM orders
 WHERE total_amt_usd < 500
 LIMIT 10;
+              
+--Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
