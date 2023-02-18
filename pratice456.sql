576fecb8f76cd443c706781aa9d459ab81627e11
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 21:42:28 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -54,4 +54,10 @@ ORDER BY 3 DESC;
 --Try using LIMIT yourself below by writing a query that displays all the data in the occurred_at, account_id, and channel columns of the web_events table, and limits the output to only the first 15 rows.
 SELECT occurred_at,account_id,channel
 FROM web_events
-LIMIT 15;              
+LIMIT 15; 
+              
+--Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at 
+LIMIT 10;
