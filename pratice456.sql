b4c7011166ff2aba0b67327205ef11c785fb3a7c
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 20:56:36 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -24,3 +24,8 @@ INNER JOIN orders AS o
 ON a.id = o.account_id; 
 
 
+--Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+SELECT CASE WHEN total >= 2000 THEN 'At least 2000' 
+            WHEN total > 1000 AND total < 2000 THEN 'Between 1000 and 2000' ELSE 'Leass than 1000' END AS categories, COUNT(*) AS order_count
+FROM orders
+GROUP BY 1;
