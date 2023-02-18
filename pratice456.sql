115396a1e5a4d74adc9b0a8fd4fb0f94516497ce
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 22:22:31 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -106,4 +106,9 @@ WHERE name = 'Exxon Mobil';
 SELECT id, account_id, (standard_amt_usd/standard_qty) AS unit_price
 FROM orders
 LIMIT 10;
+              
+--Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end with _usd. (Try to do this without using the total column.) Display the id and account_id fields also
+SELECT id, account_id, poster_amt_usd /(standard_amt_usd + gloss_amt_usd + poster_amt_usd)*100 AS poster_percentage
+FROM orders
+LIMIT 10;
 
