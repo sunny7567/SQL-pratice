036567282106c8a5175ef67475b061716edaea8e
Sunnykumar Patel <116772724+sunny7567@users.noreply.github.com>
2023-02-17 21:56:05 -0500

    Update pratice456.sql

--- pratice456.sql
+++ pratice456.sql
@@ -78,4 +78,10 @@ LIMIT 20;
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY account_id, total_amt_usd DESC;
+              
+--Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY  total_amt_usd DESC, account_id;
+              
 
